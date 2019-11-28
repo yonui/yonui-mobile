ddd9a322d027e36c02b5db386c8ce536e87cc5c4
yaoxinc <yaoxinc@yonyou.com>
2019-11-28 18:04:26 +0800

    fix: 添加flexitem

--- components/flexitem/index.tsx
+++ components/flexitem/index.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react'
-import Flex from './flex'
-export default class metFlexItem implements Flex.Item {
-  render = (props: any) => (<Flex.Item {...props}> {props.children}</Flex.Item>)
+import { Component, ComponentManifest, ReactWrapper } from '@metaui/extension'
+import FlexItem from 'antd-mobile/lib/flex/FlexItem'
+import { FlexItemPropsType } from 'antd-mobile/lib/flex/PropsType'
+
+const manifest: ComponentManifest = {
+  name: 'FlexItem',
+  label: 'flex布局下行元素FlexItem',
+  description: '实现css flex布局', // 描述
+  props: [// adapter适配层数据
+  ],
+  children: ['Button', 'Calendar', 'Card', 'Checkbox', 'Drawer', 'Flex', 'NavBar', 'Progress', 'Stepper', 'Switch', 'WhiteSpace', 'WingBlank']// 可放置哪些子组件,暂时只支持数组,不支持方法
+}
+export default class metaFlexItem implements Component<FlexItemPropsType> {
+  manifest = manifest
+  render = ReactWrapper((props: any) => <FlexItem > {props.children}</FlexItem>)
 }
