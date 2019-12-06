5193655796e50897b39f880a5bb96997e50dc657
ludl3 <ludl3@yonyou.com>
2019-12-06 13:47:12 +0800

    fix: 修改结构

--- components/label/Label.tsx
+++ components/label/Label.tsx
@@ -1,16 +1,10 @@
-/**
- * label组件,显示文本
- * props
- * label 文本内容
- * type 文本大小
- * style 自定义样式
- */
+// Label 基础组件-文本
 import React from 'react'
-import './index.less'
+import './label.less'
 export interface LabelProps {
-  type?: string
-  label?: string
-  style?: object
+  type?: string // 文本内容
+  label?: string // 文本大小
+  style?: object // 自定义样式
 }
 export default class Label extends React.Component<LabelProps> {
   render () {
