613d4a29b32389dbd78e7c1d3a3a4a67e0c62dff
HuaYJ <huayjh.yonyou.com>
2020-02-17 15:30:16 +0800

    refactor: 修改结构，修改了button组件和demo

---
+++ src/components/_DEMO/demos/demo.js
@@ -0,0 +1,15 @@
+/**
+ * @name: Button
+ * @description: sss
+ */
+import React, { Component } from 'react';
+import MyComponent from '../index';
+import '../style';
+import './demo.less';
+export default class Demo1 extends Component {
+  render () {
+    return (
+      <MyComponent type='primary'>Button</MyComponent>
+    )
+  }
+}
