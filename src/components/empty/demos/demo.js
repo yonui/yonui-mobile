f01831a073b3711b57ddfa61e864ce92874caf15
yanglinz <yanglinz@yonyou.com>
2020-02-18 15:43:20 +0800

    修改添加布局组件

---
+++ src/components/empty/demos/demo.js
@@ -0,0 +1,17 @@
+/**
+ * @name: Button
+ * @description: sss
+ */
+import React, { Component } from 'react';
+import Result from '../index';
+import '../style';
+import './demo.less';
+export default class Demo extends Component {
+  render () {
+    return (
+      <Result
+
+    />
+    )
+  }
+}
