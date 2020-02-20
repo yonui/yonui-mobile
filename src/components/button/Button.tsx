d71530d353b0d4d55b9ab9ae0dca48c21ea44509
nizhja <nzj19910102@163.com>
2020-02-20 16:03:58 +0800

    fix: 修改按钮样式

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,59 +1,37 @@
-import React from 'react';
-import { Button } from 'antd-mobile';
-import classnames from 'classnames';
+import React from 'react'
+import { Button } from 'antd-mobile'
 export interface ButtonProps {
     prefixCls?: string;
     className?: string;
     type?: string;
     size?: string;
     loading?: boolean;
-    content?: React.ReactElement| string;
+    content?: React.ReactElement | string;
     disabled?: boolean;
     inline?: boolean;
     ghost?: boolean;
     onClick?: React.MouseEventHandler<HTMLAnchorElement>;
 }
-const _prefixCls='am-button';
 export default class MDFButton extends React.Component<ButtonProps, any> {
   static defaultProps = {
-    prefixCls: {_prefixCls},
+    prefixCls: 'am-button',
     type: 'default',
     size: 'large',
-    loading:false,// 是否显示加载图标
-    content: <div>Button</div>,
+    loading: false, // 是否显示加载图标
+    content: 'Button',
     disabled: false,
     inline: false,
     ghost: false
   }
   constructor(props: ButtonProps) {
-      super(props)
+    super(props)
   }
-    getClassName = () => {
-      const { type, size,loading,content, ghost, inline, disabled } = this.props
-
-    let _className;
-    // debugger;
-    if (ghost) {
-      return classnames(this.props.className, `${ _prefixCls}-ghost`)
-    }
-    if (size === 'small') {
-      _className = classnames(this.props.className, `${_prefixCls}-${this.props.type}-${this.props.size}`)
-    } else {
-      _className = classnames(this.props.className, `${_prefixCls}-${this.props.type}`)
-    }
-    // debugger;
-    return _className
-  }
-
   render() {
-    let _className =this.getClassName();
-      return (
-        <Button className = {_className}>
-          {this.props.content || this.props.children}
-        </Button>
-
-      )
-
+    return (
+      <Button { ...this.props}>
+        {this.props.content || this.props.children}
+      </Button>
+    )
   }
 }
 
