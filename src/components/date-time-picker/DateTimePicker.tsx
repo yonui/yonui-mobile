8f3054d2d3f19c3d55f0321ea6bfc24214e0cff1
HuaYJ <huayjh@yonyou.com>
2020-03-31 10:43:39 +0800

    feat: date-time-picker组件

---
+++ src/components/date-time-picker/DateTimePicker.tsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import { List, DatePicker, Calendar } from 'antd-mobile'
+import { DatePickerPropsType } from 'antd-mobile/lib/date-picker/PropsType'
+import { dateFormat } from '../_utils'
+export interface ListDatePickerProps extends DatePickerPropsType {
+  label?: string
+  required?: boolean
+  arrow?: boolean
+  style?: object
+  dateMode?: 'picker-date' | 'picker-time' | 'picker-datetime' | 'picker-year' | 'picker-month' | 'calendar-date' | 'calendar-datetime'
+  onConfirm?: (dateTime?: Date) => void
+  onCancel?: () => void
+}
+
+interface ListDatePickerState {
+  visible?: boolean
+}
+class ListDatePicker extends React.Component<ListDatePickerProps, ListDatePickerState> {
+  static defaultProps = {
+    arrow: true,
+    dateMode: 'picker-date'
+  }
+
+  state = {
+    visible: false
+  }
+
+  onOpenCalendar = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  onCancel = () => {
+    this.props.onCancel && this.props.onCancel()
+    this.setState({
+      visible: false
+    })
+  }
+
+  handClick = () => {
+    const { disabled } = this.props
+    if (disabled) {
+      return
+    }
+    this.setState({
+      visible: true
+    })
+  }
+
+  onConfirm = (dateTime?: Date) => {
+    this.props.onConfirm && this.props.onConfirm(dateTime)
+    this.setState({
+      visible: false
+    })
+    console.log(dateTime)
+  }
+
+  render () {
+    const { label, required, value, minDate, maxDate, arrow, disabled, style, dateMode, onConfirm, onCancel, format, extra, title, ...restProps } = this.props
+    const { visible } = this.state
+    const requiredCls = required ? 'required' : ''
+    const valueTrs = (value && typeof value === 'string') ? new Date(value) : value
+    const minDateTrs = (minDate && typeof minDate === 'string') ? new Date(minDate) : minDate
+    const maxDateTrs = (maxDate && typeof maxDate === 'string') ? new Date(maxDate) : maxDate
+    const typeAndMode = dateMode?.split('-') || []
+    // const fmt = (format && typeof format === 'string') ? format : ((format && value ? format(value) : 'yyyy-MM-dd'))
+    const fmt = format ? (typeof format === 'string' ? format : (value && format(value))) : 'yyyy-MM-dd'
+    if (typeAndMode[0] === 'picker') {
+      return <List className={`date-time-picker ${(disabled || !arrow) && 'no-arrow'}`} style={style}>
+        <DatePicker
+          {...restProps}
+          title={title}
+          extra={extra}
+          format={format}
+          disabled={disabled}
+          value={valueTrs}
+          minDate={minDateTrs}
+          maxDate={maxDateTrs}
+          mode={typeAndMode[1]}
+          onOk={this.onConfirm}
+          onDismiss={this.onCancel}
+        >
+          <List.Item
+            arrow={arrow ? 'horizontal' : ''}
+            className={`from-label ${requiredCls}`}>
+            {label}
+          </List.Item>
+        </DatePicker>
+      </List>
+    } else if (typeAndMode[0] === 'calendar') {
+      return (<List className='list-calendar' style={style}>
+        <List >
+          <List.Item
+            arrow={arrow ? 'horizontal' : ''}
+            className={`from-label ${requiredCls}`}
+            onClick = {!disabled ? this.onOpenCalendar : undefined}
+            extra = { valueTrs ? dateFormat(valueTrs, fmt || 'yyyy-MM-dd') : extra}
+          >
+            {label}
+          </List.Item>
+        </List>
+        <Calendar
+          // {...restProps}
+          title='title'
+          minDate={minDateTrs}
+          maxDate={maxDateTrs}
+          visible = {visible}
+          onCancel={this.onCancel}
+          onConfirm={this.onConfirm}
+          pickTime={typeAndMode[1] === 'datetime'}
+          type='one'
+        />
+      </List>)
+    }
+    return (
+      <List className={`date-time-picker ${(disabled || !arrow) && 'no-arrow'}`} style={style}>
+        <DatePicker
+          {...restProps}
+          disabled={disabled}
+          value={valueTrs}
+          minDate={minDateTrs}
+          maxDate={maxDateTrs}>
+          <List.Item
+            arrow={arrow ? 'horizontal' : ''}
+            className={`from-label ${requiredCls}`}>
+            {label}
+          </List.Item>
+        </DatePicker>
+      </List>
+    )
+  }
+}
+export default ListDatePicker
---
+++ src/components/date-time-picker/manifest.tsx
@@ -0,0 +1,240 @@
+import { FieldTypes, EditTypes, ComponentManifest, UIObject } from 'yonui-extension'
+const manifest: ComponentManifest = {
+  name: 'DateTimePicker',
+  label: '列表项日期选择',
+  type: 'basicControls',
+  uiObject: UIObject.Controls,
+  icon: 'list',
+  props: [
+    {
+      name: 'label',
+      type: FieldTypes.string,
+      defaultValue: '日期选择',
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: true,
+        props: {},
+        label: '标签'
+      }
+    },
+    {
+      name: 'dateMode',
+      type: FieldTypes.string,
+      defaultValue: 'picker-date',
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Select,
+        isRequired: true,
+        props: {
+          options: [
+            { value: 'picker-date', text: '选择器-日期' },
+            { value: 'picker-time', text: '选择器-时间' },
+            { value: 'picker-datetime', text: '选择器-日期时间' },
+            { value: 'picker-year', text: '选择器-年' },
+            { value: 'picker-month', text: '选择器-年月' },
+            { value: 'calendar-datetime', text: '日历-日期时间' },
+            { value: 'calendar-date', text: '日历-日期' }
+          ]
+        },
+        label: '选择类型'
+      }
+    },
+    {
+      name: 'value',
+      type: FieldTypes.date,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Date,
+        isRequired: true,
+        props: {},
+        label: '选中时间'
+      }
+    },
+    {
+      name: 'minDate',
+      type: FieldTypes.date,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Date,
+        isRequired: false,
+        props: {},
+        label: '最小可选日期'
+      }
+    },
+    {
+      name: 'maxDate',
+      type: FieldTypes.date,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Date,
+        isRequired: false,
+        props: {},
+        label: '最大可选日期'
+      }
+    },
+    {
+      name: 'use12Hours',
+      type: FieldTypes.boolean,
+      defaultValue: false,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Bool,
+        isRequired: false,
+        props: {},
+        label: '12小时制'
+      }
+    },
+    {
+      name: 'minuteStep',
+      type: FieldTypes.number,
+      defaultValue: 1,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Number,
+        isRequired: false,
+        props: {},
+        label: '递增步长',
+        help: '分钟数递增步长设置'
+      }
+    },
+    {
+      name: 'required',
+      type: FieldTypes.boolean,
+      defaultValue: false,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Bool,
+        isRequired: false,
+        props: {},
+        label: '必填'
+      }
+    },
+    {
+      name: 'arrow',
+      type: FieldTypes.boolean,
+      defaultValue: true,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Bool,
+        isRequired: false,
+        props: {},
+        label: '显示箭头'
+      }
+    },
+    {
+      name: 'disabled',
+      type: FieldTypes.boolean,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Bool,
+        isRequired: false,
+        props: {},
+        label: '禁用'
+      }
+    },
+    {
+      name: 'onChange',
+      type: FieldTypes.action,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: false,
+        props: {},
+        label: 'onChange'
+      }
+    },
+    {
+      name: 'onValueChange',
+      type: FieldTypes.action,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: false,
+        props: {},
+        label: 'onValueChange',
+        help: '每列picker改变时的回调'
+      }
+    },
+    {
+      name: 'format',
+      type: FieldTypes.string,
+      defaultValue: 'YYYY-MM-DD',
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Select,
+        isRequired: true,
+        props: {
+          options: [
+            { value: 'YYYY-MM-DD', text: '日期' },
+            { value: 'HH:mm', text: '时间' },
+            { value: 'YYYY-MM-DD HH:mm', text: '日期+时间' }
+          ]
+        },
+        label: 'format',
+        help: '格式化选中的值'
+      }
+    },
+    {
+      name: 'title',
+      type: FieldTypes.string,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: false,
+        props: {},
+        label: '弹框标题'
+      }
+    },
+    {
+      name: 'extra',
+      type: FieldTypes.string,
+      defaultValue: '请选择',
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: false,
+        props: {},
+        label: '显示文案'
+      }
+    },
+    // {
+    //   name: 'prefixCls',
+    //   type: FieldTypes.string,
+    //   defaultValue: 'am-picker',
+    //   showDesign: true,
+    //   designConfig: {
+    //     type: EditTypes.Text,
+    //     isRequired: false,
+    //     props: {},
+    //     label: 'class前缀'
+    //   }
+    // },
+    {
+      name: 'onOk',
+      type: FieldTypes.action,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: false,
+        props: {},
+        label: 'onOk',
+        help: '选中回调'
+      }
+    },
+    {
+      name: 'onDismiss',
+      type: FieldTypes.action,
+      showDesign: true,
+      designConfig: {
+        type: EditTypes.Text,
+        isRequired: false,
+        props: {},
+        label: 'onDismiss',
+        help: '取消回调'
+      }
+    }
+  ]
+}
+
+export default manifest
