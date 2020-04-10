407aeef8aaf8fb34c71d85356ea7e007477dd26d
nizhja <nzj19910102@163.com>
2020-04-10 16:09:06 +0800

    fix: 修改checkbox

--- src/components/checkbox/CheckBox.tsx
+++ src/components/checkbox/CheckBox.tsx
@@ -20,12 +20,17 @@ interface CheckboxControlProps extends React.defaultProps{
   onChange?: () => void
 }
 export default class CheckboxControl extends Component<CheckboxControlProps> {
+  onChange = (event: any) => {
+    const { onChange } = this.props
+    onChange && onChange(event && event.target && event.target.checked)
+  }
+
   render () {
     const { className, mode, content, children, nid, uitype, ...other } = this.props
     const cls = classnames(className, `${checkboxWarpPrefixCls}-${mode}`)
     return (
       <span className='yonui-checkbox-wrapper' nid={nid} uitype={uitype}>
-        <Checkbox className={cls} {...other}>{content}</Checkbox>
+        <Checkbox className={cls} onChange={this.onChange} {...other}>{content}</Checkbox>
       </span>
     )
   }
