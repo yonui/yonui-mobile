44196b559ddbca669a5aa2357482f66245bc976f
HuaYJ <huayjh@yonyou.com>
2020-04-16 18:55:08 +0800

    fix: 修改input组件

--- src/components/input-yonui/InputYonui.tsx
+++ src/components/input-yonui/InputYonui.tsx
@@ -6,14 +6,21 @@ interface InputYonuiProps extends React.defaultProps{
   finalPattern?: RegExp // onBlur时的校验规则
   value?: string
   textAlign?: 'left' | 'right' | 'center'
-  onChange?: (value?: string) => void
-  onError?: (value?: string) => void
+  placeholder?: string
+  maxLength?: number
+  inputStyle?: React.CSSProperties
+  onFocus?: (value: string) => void
+  onBlur?: (value: string) => void
+  onChange?: (value: string) => void
+  onError?: (value: string) => void
+  onSuccess?: (value: string) => void
   beforeRender?: (value: string) => string
   afterChange?: (value: string) => string
 }
 
 interface InputYonuiState {
   _value: string
+  _className: string
 }
 export default class InputYonui extends Component<InputYonuiProps, InputYonuiState> {
   static defaultProps = {
@@ -21,14 +28,23 @@ export default class InputYonui extends Component<InputYonuiProps, InputYonuiSta
   }
 
   state = {
-    _value: ''
+    _value: '',
+    _className: ''
   }
 
-  _onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const currentValue = event.target.value
-    const { onChange, pattern, afterChange } = this.props
+  checkValue = (value: string) => {
+    const { maxLength, pattern } = this.props
+
+    if (pattern && !pattern.test(value)) return false
+    if (maxLength && value.length > maxLength) return false
+    return true
+  }
+
+  _onChange = (event: React.ChangeEvent<HTMLInputElement> | '') => {
+    const currentValue = event ? event.target.value : ''
+    const { onChange, afterChange } = this.props
     const val = afterChange ? afterChange(currentValue) : currentValue
-    if (pattern && !pattern.test(val)) {
+    if (!this.checkValue(val)) {
       return
     }
     onChange && onChange(val)
@@ -39,23 +55,53 @@ export default class InputYonui extends Component<InputYonuiProps, InputYonuiSta
 
   _onBlur = (event: React.FocusEvent<HTMLInputElement>) => {
     const { _value } = this.state
-    const { finalPattern, onError } = this.props
+    const { finalPattern, onError, onBlur, onSuccess } = this.props
     if (finalPattern && !finalPattern.test(_value)) {
       onError && onError(_value)
       console.warn('the result does not match the rule:', finalPattern.toString())
+    } else {
+      onSuccess && onSuccess(_value)
     }
+    onBlur && onBlur(_value)
+    setTimeout(() => {
+      this.setState({
+        _className: ''
+      })
+    }, 100)
+  }
+
+  _onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    const { onFocus } = this.props
+    onFocus && onFocus(event.target.value)
+    this.setState({
+      _className: 'yonui-input-focus'
+    })
+  }
+
+  onClickClear = () => {
+    const { onChange } = this.props
+    onChange && onChange('')
+    this.setState({
+      _value: ''
+    })
   }
 
   render () {
-    const { className, style, type, value, textAlign, beforeRender } = this.props
-    const { _value } = this.state
+    const { className, style, type, value, textAlign, beforeRender, placeholder, inputStyle } = this.props
+    const { _value, _className } = this.state
     const val = value || _value
     const displayValue = beforeRender ? beforeRender(val) : val
-    const cls = classnames(className, 'yonui-input')
-    const inputStyle: React.CSSProperties = { textAlign }
+    const cls = classnames(className, 'yonui-input', {
+      [_className]: val.length > 0
+    })
+    const _inputStyle: React.CSSProperties = { textAlign, ...inputStyle }
     return (
       <div className={cls} style={style}>
-        <input className='yonui-input-box' type={type} value={displayValue} onChange={this._onChange} onBlur={this._onBlur} style={inputStyle}/>
+        <input className='yonui-input-box' type={type} value={displayValue}
+          onChange={this._onChange} onBlur={this._onBlur} style={_inputStyle}
+          onFocus={this._onFocus} placeholder={placeholder}
+        />
+        <div className='yonui-clear' onClick={this.onClickClear}></div>
       </div>
     )
   }
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,33 +1,102 @@
 import React, { Component } from 'react'
-import { InputItem } from 'antd-mobile'
-import { InputItemPropsType } from 'antd-mobile/lib/input-item/PropsType'
+// import { InputItem } from 'antd-mobile'
+// import { InputItemPropsType } from 'antd-mobile/lib/input-item/PropsType'
+import YonuiInput from '../input-yonui'
 import classnames from 'classnames'
 import Wrapper from '../list-item-wrapper'
-export interface InputProps extends InputItemPropsType, React.defaultProps {
+export interface InputProps extends React.defaultProps {
   label?: string
   required?: boolean
   splitLine?: boolean
   className?: string
   style?: React.CSSProperties
   singleLine?: boolean
+  maxLength?: number
+  subuitype?: 'text' | 'idCard' | 'email' | 'ipAddress' | 'bankCard16' | 'bankCard19' | 'customized'
+  regRule?: string | RegExp
+  check?: boolean
+  onFocus?: (value: string) => void
+  onBlur?: (value: string) => void
+  onChange?: (value: string) => void
+  onError?: (value: string) => void
+  onSuccess?: (value: string) => void
 }
-export default class Input extends Component<InputProps> {
-  constructor (props: InputProps) {
-    super(props)
-    this.state = {}
+
+interface InputState {
+  error?: boolean
+}
+export default class Input extends Component<InputProps, InputState> {
+  state = {
+    error: false,
+    check: false
   }
 
   static defaultProps = {
     // singleLine: false
+    // maxLength: 255,
+    subuitype: 'text'
+  }
+
+  getInputProps = () => {
+    const { subuitype, onError, onSuccess, check } = this.props
+    const _onError = (value: string) => {
+      onError && onError(value)
+      this.setState({
+        error: check && true
+      })
+    }
+    const _onSuccess = (value: string) => {
+      onSuccess && onSuccess(value)
+      this.setState({
+        error: check && false
+      })
+    }
+    let res: any = {
+      onSuccess: _onSuccess,
+      onError: _onError
+    }
+    switch (subuitype) {
+      case 'idCard': {
+        res = {
+          ...res,
+          text: 'tel',
+          pattern: /^[0-9xX]*$/,
+          finalPattern: /^[1-9]\d{5}(?:18|19|20)\d{2}(?:(?:0[1-9])|(?:1[0-2]))(?:(?:[0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/,
+          maxLength: 18
+        }
+        break
+      }
+      case 'ipAddress': {
+        res = {
+          ...res,
+          text: 'number',
+          pattern: /^[0-9.]*$/,
+          finalPattern: /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/,
+          maxLength: 15
+        }
+        break
+      }
+      case 'text':
+      default: {
+        res = {
+          type: 'text'
+        }
+        break
+      }
+    }
+    return res
   }
 
   render () {
     const { label, required, splitLine, className, singleLine, style, nid, uitype, ...other } = this.props
+    const { error } = this.state
     const cls = classnames('mdf-input', className)
     const inputCls = classnames('mdf-input-content')
+    const inputProps = this.getInputProps()
     return (
-      <Wrapper className={cls} style={style} splitLine={splitLine} singleLine={singleLine} nid={nid} uitype={uitype} label={label} required={required} >
-        <InputItem className={inputCls} {...other} labelNumber={0} />
+      <Wrapper className={cls} style={style} splitLine={splitLine} singleLine={singleLine} nid={nid} uitype={uitype} label={label} required={required} error={error}>
+        <YonuiInput className={inputCls} {...other} {...inputProps} textAlign={singleLine ? 'right' : 'left'}
+        />
       </Wrapper>
     )
   }
