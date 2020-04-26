12f1bd5fb2613c557982b290e70e5ae0a0253385
HuaYJ <huayjh@yonyou.com>
2020-04-26 15:28:44 +0800

    fix: textarea组件新增必填校验

--- src/components/textarea-item/TextareaItem.tsx
+++ src/components/textarea-item/TextareaItem.tsx
@@ -8,19 +8,33 @@ interface TextareaProps extends TextAreaItemPropsType, React.defaultProps{
   required?: boolean
   maxLength?: number
   splitLine?: boolean
+  errorText?: string
 }
-export default class MyComponent extends Component<TextareaProps> {
+interface TextareaState {
+  requiredError: boolean
+}
+export default class MyComponent extends Component<TextareaProps, TextareaState> {
   constructor (props: TextAreaItemPropsType) {
     super(props)
-    this.state = {}
+    this.state = {
+      requiredError: false
+    }
+  }
+
+  _onBlur = (value: any) => {
+    const { required } = this.props
+    this.setState({
+      requiredError: !!required && !value
+    })
   }
 
   render () {
-    const { label, className, style, nid, uitype, required, maxLength, splitLine, rows = 3, ...other } = this.props
+    const { label, className, style, nid, uitype, required, maxLength, splitLine, rows = 3, errorText, ...other } = this.props
+    const { requiredError } = this.state
     const cls = classnames(className, 'yonui-textarea')
     return (
-      <Wrapper className={cls} style={style} uitype={uitype} nid={nid} splitLine={splitLine} label={label} required={required}>
-        <TextareaItem rows={rows} {...other} count={maxLength}/>
+      <Wrapper className={cls} style={style} uitype={uitype} nid={nid} splitLine={splitLine} label={label} required={required} error={requiredError} errorText={errorText}>
+        <TextareaItem rows={rows} {...other} count={maxLength} onBlur={this._onBlur}/>
       </Wrapper>
     )
   }
