7701927b50cfcc38903d666de2e1cc9548c9be04
HuaYJ <huayjh@yonyou.com>
2020-05-26 16:14:50 +0800

    fix: checkbox设置默认值属性

--- src/components/checkbox/manifest.tsx
+++ src/components/checkbox/manifest.tsx
@@ -38,14 +38,14 @@ const manifest: ComponentManifest = {
       }
     },
     {
-      name: 'defaultChecked',
+      name: 'cDefaultValue',
       type: FieldTypes.boolean,
       showDesign: true,
       designConfig: {
         type: EditTypes.Bool,
         isRequired: true,
         props: {},
-        label: '初始选中'
+        label: '默认值'
       }
     },
     {
@@ -83,79 +83,9 @@ const manifest: ComponentManifest = {
       }
     }
   ],
-  children: []
+  children: [],
+  propertyMap: {
+    cDefaultValue: 'defaultChecked'
+  }
 }
-
-// const manifestCheckBoxItem: ComponentManifest = {
-//   name: 'CheckboxItem',
-//   label: '复选框',
-//   description: '',
-//   type: 'basicControls',
-//   uiObject: UIObject.Controls,
-//   props: [
-//     {
-//       name: 'defaultChecked',
-//       type: FieldTypes.boolean,
-//       showDesign: true,
-//       designConfig: {
-//         type: EditTypes.Bool,
-//         isRequired: true,
-//         props: {},
-//         label: '初始值'
-//       }
-//     },
-//     {
-//       name: 'checked',
-//       type: FieldTypes.boolean,
-//       showDesign: true,
-//       designConfig: {
-//         type: EditTypes.Bool,
-//         isRequired: true,
-//         props: {},
-//         label: '值'
-//       }
-//     },
-//     {
-//       name: 'disabled',
-//       type: FieldTypes.boolean,
-//       defaultValue: false,
-//       showDesign: true,
-//       designConfig: {
-//         type: EditTypes.Bool,
-//         isRequired: true,
-//         props: {},
-//         label: '禁用'
-//       }
-//     },
-//     {
-//       name: 'onChange',
-//       type: FieldTypes.action,
-//       showDesign: true,
-//       designConfig: {
-//         type: EditTypes.Text,
-//         isRequired: true,
-//         props: {},
-//         label: 'onChange'
-//       }
-//     }
-//   ],
-//   children: []
-// }
-
-// const manifestAgreeItem: ComponentManifest = {
-//   name: 'AgreeItem',
-//   label: 'AgreeItem',
-//   description: '', // 描述
-//   type: 'basicControls',
-//   uiObject: UIObject.Controls,
-//   props: [// adapter适配层数据
-//   ]
-// }
-// export {
-//   manifest,
-//   ReactWrapper,
-//   manifestCheckBoxItem,
-//   manifestAgreeItem
-// }
-
 export default manifest
