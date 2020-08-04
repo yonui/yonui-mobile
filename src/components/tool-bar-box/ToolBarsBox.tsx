9f46cc3c5c62aea26dc3cd2c117d9e9d6860f313
Zhangyhak <924424708@qq.com>
2020-08-04 10:54:47 +0800

    tool-bar-box滑动

--- src/components/tool-bar-box/ToolBarsBox.tsx
+++ src/components/tool-bar-box/ToolBarsBox.tsx
@@ -16,13 +16,20 @@ export default class ToolBarBox extends Component<ToolBarBoxProps> {
     const childrenLength = Array.isArray(children) ? Math.min(children.length, 5) : 1
     // const _length = layout === 'vertical' ? 1 : childrenLength
     const wrapperCls = classnames('yonui-mobile-tool-bars-wrapper')
-    const cls = classnames(className, 'yonui-mobile-tool-bars-2', layout, `length-${childrenLength}`, {
+    const cls = classnames(className, 'yonui-mobile-tool-bars-2', layout, {
       'yonui-mobile-tool-bars-runtime': runTime
     })
+    const btnNum = Array.isArray(children) ? children.length : 1
+    const newChildren = []
+    if (btnNum !== 1) {
+      for (let i = 0; i < btnNum; i++) {
+        newChildren[i] = <div className={classnames(`length-${childrenLength}`)}>{children[i]}</div>
+      }
+    } else newChildren[0] = <div className={classnames(`length-${childrenLength}`)}>{children}</div>
     return (
       <div className={cls} style={style} nid={nid} uitype={uitype}>
         <div className={wrapperCls}>
-          {children}
+          {newChildren}
         </div>
       </div>
     )
