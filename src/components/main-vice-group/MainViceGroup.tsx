80378f59dd715bf01a7c97b44fdf5ab5c94507ff
张熠辉 <zhangyhak@yonyou.com>
2020-09-07 18:14:51 +0800

    fix: 页面抽屉

--- src/components/main-vice-group/MainViceGroup.tsx
+++ src/components/main-vice-group/MainViceGroup.tsx
@@ -16,25 +16,14 @@ interface MainViceGroupProps {
 export default class MainViceGroup extends Component<MainViceGroupProps> {
   componentDidMount () {
     this.props.onShow && this.props.onShow()
-    if (this.props.groupType === 'drawer') {
-      this.setDrawerSize(this.props.drawerWidth, this.props.drawerHeight)
-    }
   }
 
   componentWillMount () {
     // this.props.onPageWillUnmount && this.props.onPageWillUnmount()
   }
 
-  setDrawerSize = (width: number, height: number) => {
-    const drawerA = document.querySelector('[uitype="groupcontainer"]')
-    const drawerR = document.querySelector('.ant-drawer-content-wrapper')
-    const newStyleText = `width: ${width}px; height: ${height}px; min-height: 0px`
-    drawerA && drawerA.setAttribute('style', newStyleText)
-    drawerR && drawerR.setAttribute('style', newStyleText)
-  }
-
   render () {
-    const { children, className, style, groupType, drawerWidth, drawerHeight } = this.props
+    const { children, className, style, drawerHeight } = this.props
     let customStyle: React.CSSProperties = {}
     if (style) {
       customStyle = style
@@ -43,9 +32,6 @@ export default class MainViceGroup extends Component<MainViceGroupProps> {
       if (!Object.prototype.hasOwnProperty.call(customStyle, 'height')) {
         customStyle.height = `${drawerHeight}px`
       }
-    }
-    if (groupType === 'drawer') {
-      this.setDrawerSize(drawerWidth, drawerHeight)
     }
     const cls = classnames([className, 'am-c-group-container'])
     return (
