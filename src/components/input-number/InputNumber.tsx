baa2144b511bf50c04aafd045b44b506471d2aa1
张熠辉 <zhangyhak@yonyou.com>
2020-11-18 16:43:35 +0800

    fix: 数值控件允许为空时校验报错

--- src/components/input-number/InputNumber.tsx
+++ src/components/input-number/InputNumber.tsx
@@ -218,7 +218,9 @@ export default class Input extends Component<InputProps, InputState> {
     const inputCls = classnames('mdf-input-content')
     const inputProps = this.getInputProps()
     const wrapperProps = getListItemProps(this.props, { className: cls, error, errorText })
-    const _required = bIsNull !== undefined ? bIsNull : required
+    const _required = bIsNull !== undefined ? !bIsNull : required
+    console.log('------------inputnumber-render--------------------')
+    console.log('required', required, 'bisnull', bIsNull, '_required', _required)
     const preValue = this.state.isFocus ? value : this.changeValue(this.props);
     const showValue = mReadOnly ? preValue : (this.state.isFocus ? _displayValue || preValue : preValue)
     if (!visible) return null
