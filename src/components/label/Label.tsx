2803f3a2e1391ed9c16748f2b52327ba5e50ba12
张熠辉 <zhangyhak@yonyou.com>
2020-11-18 17:12:04 +0800

    fix: 标签绑定日期选择字段不应显示时分秒

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -71,44 +71,48 @@ export default class Label extends React.PureComponent<LabelProps> {
 
   adaptValue = (label: string) => {
     const { controlType, dataSource, multiple } = this.props
-    if (controlType === 'switch') {
-      return label ? '是' : '否'
-    } else if (controlType === 'optionwidget') {
-      if (dataSource) {
-        try {
-          if (dataSource?.slice(0, 1) === '{' && dataSource?.slice(-1) === '}') {
-            const obj = this.parseValue(dataSource)
-            if (multiple) {
-              if (label.includes(',')) {
-                const items = label.split(',')
-                let result = ''
-                items.forEach(element => {
-                  const value = obj[element]
-                  result = result.concat(`${value},`)
-                })
-                return result.replace(/^,+|,+$/g, '')
+    switch (controlType) {
+      case 'switch':
+        return label ? '是' : '否'
+      case 'optionwidget':
+        if (dataSource) {
+          try {
+            if (dataSource?.slice(0, 1) === '{' && dataSource?.slice(-1) === '}') {
+              const obj = this.parseValue(dataSource)
+              if (multiple) {
+                if (label.includes(',')) {
+                  const items = label.split(',')
+                  let result = ''
+                  items.forEach(element => {
+                    const value = obj[element]
+                    result = result.concat(`${value},`)
+                  })
+                  return result.replace(/^,+|,+$/g, '')
+                } else {
+                  return obj[label]
+                }
               } else {
                 return obj[label]
               }
-            } else {
-              return obj[label]
             }
+          } catch (e) {
+            return label
+          }
+        }
+        break
+      case 'datepicker':
+        return label?.split(' ')[0]
+      default :
+        try {
+          if (label?.slice(0, 1) === '{' && label?.slice(-1) === '}') {
+            const obj = this.parseValue(label)
+            return obj.address
+          } else {
+            return label
           }
         } catch (e) {
           return label
         }
-      }
-    } else {
-      try {
-        if (label?.slice(0, 1) === '{' && label?.slice(-1) === '}') {
-          const obj = this.parseValue(label)
-          return obj.address
-        } else {
-          return label
-        }
-      } catch (e) {
-        return label
-      }
     }
   }
 
