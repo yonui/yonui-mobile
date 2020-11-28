55b50e7cc14d18634277146d3407855b020c3f75
张熠辉 <zhangyhak@yonyou.com>
2020-11-28 11:04:59 +0800

    style: 多选/日期样式

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -3,10 +3,10 @@ import { Switch } from 'antd-mobile'
 import ListItemWrapper, { getListItemProps } from '../list-item-wrapper'
 import classnames from 'classnames'
 
-const getValue = (value: string | boolean, defaultValue: string) => {
+const getValue = (value: string | boolean, defaultValue: boolean) => {
   if (value === true || value === 'true') return true
   if (value === false || value === 'false') return false
-  return defaultValue === 'true'
+  return defaultValue
 }
 export default class SwitchControl extends React.Component<any> {
   render () {
