73456021d9adf2d16d01433b65c1a228ee213f60
张熠辉 <zhangyhak@yonyou.com>
2021-01-20 14:57:14 +0800

    fix: 查询方案中点击重置开关未重置PTYDD-5782

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -3,16 +3,17 @@ import { Switch } from 'antd-mobile'
 import ListItemWrapper, { getListItemProps } from '../list-item-wrapper'
 import classnames from 'classnames'
 
-const getValue = (value: string | boolean, defaultValue: string | boolean) => {
+const getValue = (value: string | boolean, defaultValue: string | boolean, model: any) => {
+  if (model?.getValue() === undefined && defaultValue === undefined) return false
   if (value === true || value === 'true') return true
-  if (value === false || value === 'false') return false
+  if (value === false || value === 'false' || value === undefined) return false
   return defaultValue === true || defaultValue === 'true'
 }
 export default class SwitchControl extends React.Component<any> {
   render () {
     // eslint-disable-next-line react/prop-types
-    const { label, required, className, defaultValue, checked, singleLine, visible = true, style, ...ohter } = this.props
-    const _checked = getValue(checked, defaultValue)
+    const { label, required, className, defaultValue, checked, singleLine, visible = true, style, model, ...ohter } = this.props
+    const _checked = getValue(checked, defaultValue, model)
     const labelCls = classnames('switch-title', 'form-label', { required })
     const wrapperProps = getListItemProps(this.props, {
       labelCls
