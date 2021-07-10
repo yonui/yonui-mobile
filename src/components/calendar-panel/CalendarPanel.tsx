98b4bf137b665347cd214889a61e7060c62a02d7
张熠辉 <zhangyhak@yonyou.com>
2021-07-10 11:24:52 +0800

    feat: zyh-calendar

--- src/components/calendar-panel/CalendarPanel.tsx
+++ src/components/calendar-panel/CalendarPanel.tsx
@@ -1,200 +1,36 @@
-import React, { Component } from 'react'
-import { Calendar } from 'antd-mobile'
-import { CalendarProps } from 'antd-mobile/lib/calendar/PropsType'
-interface YonuiCalenderProps extends CalendarProps {
-  dateExtra: any
-  onClickDay: any
-  hintDays: string[]
-}
-export default class MyComponent extends Component<YonuiCalenderProps, any> {
-  constructor (props) {
-    super(props)
-    this.adaptExtra(props.dateExtra)
-    const now = new Date()
-    this.state = {
-      visible: false,
-      extra: props.dateExtra,
-      now: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-      startY: 0,
-      touchDistance: 0,
-      monthNow: now.getMonth()
-    }
-  }
+import React from 'react'
 
-  componentDidMount () {
-    this.setState({
-      visible: true
-    })
-  }
-
-  shouldComponentUpdate (nextProps) {
-    // Object.keys(nextProps.dateExtra).length !== Object.keys(this.props.dateExtra).length
-    if (JSON.stringify(nextProps.dateExtra) !== JSON.stringify(this.props.dateExtra)) {
-      this.adaptExtra(nextProps.dateExtra)
-      this.setState({
-        visible: !this.state.visible
-      }, () => {
-        this.setState({
-          visible: !this.state.visible
-        })
-      })
-    }
-    return true
-  }
-
-  extra = []
-
-  adaptExtra = (extra = {}) => {
-    Object.keys(extra).forEach((key) => {
-      const info = extra[key];
-      const date = new Date(key);
-      if (!Number.isNaN(+date) && extra[+date] !== info) {
-        this.extra[+date] = info;
-      }
-    })
-  }
-
-  onSelect = (value, state) => {
-    let isRange = false
-    if (state[0] && state[1] === undefined) {
-      isRange = true
-    }
-    const range1 = value
-    const range2 = state[0]
-    const range = range1 < range2 ? [range1, range2] : [range2, range1]
-    const res = {
-      isRange: isRange,
-      range: range,
-      now: value
-    }
-    if (+value === +this.state.now) {
-      const info = this.extra[+value].info
-      this.extra[+value].cellRender = date => {
-        return (
-          <>
-            <div className='date-wrapper'>
-              <span className='left' />
-              <div className='date date-selected selected-single'>今</div>
-              <span className='right' />
-            </div>
-            <div className='info'>{info}</div>
-          </>
-        )
-      }
-    }
-    this.props.onClickDay?.(res)
-  }
-
-  getDateExtra = date => {
-    const now = this.state.now
-    const hintDays = this.props.hintDays || []
-    hintDays.forEach(hintDay => {
-      if (+new Date(hintDay) === +date) {
-        if (!this.extra[+date]) {
-          this.extra[+date] = {}
-        }
-        this.extra[+date].cellCls = 'lightred'
-      }
-    })
-    if (+now === +date) {
-      if (!this.extra[+date]) {
-        this.extra[+date] = {}
-        const info = this.extra[+date].info
-        this.extra[+date].cellRender = date => {
-          return (
-            <>
-              <div className='date-wrapper'>
-                <span className='left' />
-                <div className='date'>今</div>
-                <span className='right' />
-              </div>
-              <div className='info'>{info}</div>
-            </>
-          )
-        }
-      }
-    }
-    return this.extra[+date]
-  }
-
-  // onTouchStart = (e) => {
-  //   e.preventDefault();
-  //   const touch = e.touches[0]
-  //   const startY = touch.pageY
-  //   this.setState({
-  //     startY: startY
-  //   })
-  // }
-
-  // onTouchMove = (e) => {
-  //   e.preventDefault();
-  //   const touch = e.touches[0]
-  //   const moveY = touch.pageY
-  //   console.log('move', moveY)
-  // }
-
-  // onTouchEnd = (e) => {
-  //   e.preventDefault()
-  //   const singleMonth = document.querySelector('.single-month')
-  //   const touch = e.changedTouches[0]
-  //   const startY = this.state.startY
-  //   const endY = touch.pageY
-  //   let touchDistance = this.state.touchDistance
-  //   console.log('一个月距离', singleMonth.clientHeight)
-  //   if (startY - endY > singleMonth.clientHeight) {
-  //     console.log('滑动距离超过一个月', startY - endY)
-  //   }
-  //   console.log('滑动距离小于一个月', startY - endY)
-  //   touchDistance = Math.floor(touchDistance + startY - endY)
-  //   this.setState({
-  //     touchDistance: touchDistance
-  //   })
-  //   console.log('相对初始位置距离', touchDistance)
-  //   // const wrapper = document.querySelector('.wrapper')
-  //   // console.log('moveEnd', wrapper.scrollTop)
-  //   const monthStart = this.props.defaultDate.getMonth() + 1
-  //   const monthNow = monthStart + touchDistance / singleMonth.clientHeight + 1
-  //   this.props.onPull?.(monthStart, Math.floor(monthNow))
-  // }
-
-  render () {
-    const { maxDate, minDate, defaultDate, defaultTimeValue, type, defaultValue } = this.props
-    const { visible } = this.state
-    const minDateTrs = (minDate && typeof minDate === 'string') ? new Date(minDate) : minDate
-    const maxDateTrs = (maxDate && typeof maxDate === 'string') ? new Date(maxDate) : maxDate
-    const defaultDateTrs = (defaultDate && typeof defaultDate === 'string') ? new Date(defaultDate) : defaultDate
-    const defaultTimeValueTrs = (defaultTimeValue && typeof defaultTimeValue === 'string') ? new Date(defaultTimeValue) : defaultTimeValue
-    if (defaultValue?.length) {
-      defaultValue[0] = (typeof defaultValue[0] === 'string') ? new Date(defaultValue[0]) : defaultValue[0]
-      if (defaultValue[1]) {
-        if (type === 'one') {
-          delete defaultValue[1]
-          defaultValue.length = 1
-        } else {
-          defaultValue[1] = (typeof defaultValue[1] === 'string') ? new Date(defaultValue[1]) : defaultValue[1]
-        }
-      }
-    }
-    if (!visible) return null
-    return (
-      <div
-        className='am-calendar-panel'
-        // onTouchStart={this.onTouchStart}
-        // onTouchEnd={this.onTouchEnd}
-      >
-        <Calendar
-          {...this.props}
-          visible={visible}
-          onSelect={this.onSelect}
-          prefixCls='am-calendar'
-          defaultDate={defaultDateTrs}
-          defaultValue={defaultValue}
-          defaultTimeValue={defaultTimeValueTrs}
-          minDate={minDateTrs}
-          maxDate={maxDateTrs}
-          getDateExtra={this.getDateExtra}
-        />
+const getSingleMonth = (year, month) => {
+  const date = new Date(`${year}/${month}/1`)
+  const newMonth = month + 1
+  const nextDate = new Date(`${newMonth > 12 ? year + 1 : year}/${newMonth % 12}/1`)
+  const firstDay = date
+  const lastDay = new Date(nextDate.getTime() - 1000 * 60 * 60 * 24)
+  const dateNum = (lastDay.getTime() - firstDay.getTime()) / (1000 * 60 * 60 * 24) + 1
+  return (
+    <div className='single-month'>
+      <div className='row'>
+        {`${year}年${month}月, 共${dateNum}天, 第一天${firstDay}星期${firstDay.getDay()}, 最后一天${lastDay}星期${lastDay.getDay()}`}
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+export default function CalendarPanel (props) {
+  const now = new Date()
+  const fullyear = now.getFullYear()
+  const month = now.getMonth() + 1
+  const date = now.getDate()
+
+  return (
+    <div className='yonui-calendar-panel'>
+      <div className='calendar-panel-year-month'>{`${fullyear}年${month}月${date}日`}</div>
+      <div className='week-bar'>星期</div>
+      <div className='date-content'>日期
+        <div className='single-month'>一个月
+          {getSingleMonth(2021, 7)}
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/calendar-panel/demos/demo.tsx
+++ src/components/calendar-panel/demos/demo.tsx
@@ -6,67 +6,12 @@
  import MyComponent from '../index'
  import '../style'
  import './demo.less'
- import { dateFormat } from '../../_utils'
- const now = new Date()
-
- const extra = {
-   '2021/06/26':{info:'4h', disable: true},
-   '2021/06/27':{info:'4h'},
-   '2021/06/28':{info:'4h'},
-   '2021/07/27':{info:'4h'},
-   '2021/07/28':{info:'4h'}
- }
+ 
  export default class Demo1 extends Component<any, any> {
-   originbodyScrollY = document.getElementsByTagName('body')[0].style.overflowY;
-   constructor (props: any) {
-     super(props)
-     this.state = {
-       en: false,
-       show: false,
-       config: {},
-       extra: {},
-       pickTime: false
-     }
-   }
-
-   componentDidMount() {
-     setTimeout(() => {
-       this.setState({
-         extra: extra,
-         defaultValue: [new Date(now), new Date(now.getFullYear(), now.getMonth(), now.getDate() + 5)]
-       })
-     }, 1000)
-   }
-
-   onClickDay = (res) => {
-     console.log('res', res)
-   }
-
-   // onPull = (monthStart, monthNow) => {
-   //     console.log('获取extra', monthStart, monthNow)
-   //     setTimeout(() => {
-   //       this.setState({
-   //         extra: {'2021/8/2': {info: '新数据'}},
-   //         // defaultDate: new Date('2021/8/8')
-   //        })
-   //     })
-   // }
-
    render () {
      return (
        <div style={{ height: '100%' }} >
-         <MyComponent
-           visible={true}
-           // defaultDate={now}
-           // defaultValue={this.state.defaultValue}
-           defaultDate={this.state.defaultDate || now}
-           dateExtra={this.state.extra}
-           onClickDay={this.onClickDay}
-           // onPull={this.onPull}
-           initalMonths={6}
-           maxDate={'2021-8-5'}
-           hintDays={["2021-6-3","2021-6-4"]}
-         />
+         <MyComponent />
        </div>
      )
    }
