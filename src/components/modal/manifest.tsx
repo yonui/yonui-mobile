d11da848fc07fb6faa32576582975d9c0b73be6b
李鹏飞 <lipfs@yonyou.com>
2021-12-07 20:25:50 +0800

    fix: modal组件

---
+++ src/components/modal/manifest.tsx
@@ -0,0 +1,9 @@
+import { ComponentManifest, UIObject } from 'yonui-extension'
+
+const manifest: ComponentManifest = {
+  name: 'Modal', // 自定义组件名称
+  label: '对话框', // 自定义组件文本
+  uiObject: UIObject.Controls,
+  icon: 'title'
+}
+export default manifest
